26c968e40376dcabd910cd7112a685bfaf43ab99
Carlos Carral C <carloscarral13@gmail.com>
2020-12-25 12:16:46 -0600

    Pre-automatic meta tags

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,11 +1,20 @@
+const title = "📷 🐶 🖥️";
+const description = "Cosas que sólo me interesan a mí.";
+
 module.exports = {
-  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  head: [
+    ["meta", { rel: "icon", href: "/favicon.ico" }],
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["meta", { property: "og:description", content: description }],
+    ["meta", { property: "og:image", content: "/setup3.jpg" }],
+    ["meta", { property: "twitter:card", content: "summary_large_image" }]
+  ],
 
   locales: {
     "/": {
       lang: "es-MX", // this will be set as the lang attribute on <html>
-      title: "📷 🐶 🖥️",
-      description: "Cosas que sólo me interesan a mí"
+      title,
+      description
     }
   },
 
